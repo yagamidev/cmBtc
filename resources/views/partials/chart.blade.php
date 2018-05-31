4a5695152852e754eee50b17cf9be005e523feaa
yagamidev <yagamidev@gmail.com>
2018-05-31 20:03:37 +0100

    Separating chart view from main page

---
+++ resources/views/partials/chart.blade.php
@@ -0,0 +1,110 @@
+					<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js" async>
+					{
+					"title_raw": "Cryptocurrencies",
+					"belowLineFillColorGrowing": "rgba(60, 188, 152, 0.05)",
+					"gridLineColor": "rgba(233, 233, 234, 1)",
+					"scaleFontColor": "rgba(218, 221, 224, 1)",
+					"title": "Cryptocurrencies",
+					"tabs": [
+						{
+						"title_raw": "Overview",
+						"symbols": [
+							{
+							"s": "BITFINEX:BTCUSD"
+							},
+							{
+							"s": "BITFINEX:ETHUSD"
+							},
+							{
+							"s": "COINBASE:BCHUSD"
+							},
+							{
+							"s": "COINBASE:LTCUSD"
+							}
+						],
+						"title": "Overview"
+						},
+						{
+						"title_raw": "Bitcoin",
+						"symbols": [
+							{
+							"s": "BITFINEX:BTCUSD"
+							},
+							{
+							"s": "COINBASE:BTCEUR"
+							},
+							{
+							"s": "COINBASE:BTCGBP"
+							},
+							{
+							"s": "BITFLYER:BTCJPY"
+							},
+							{
+							"s": "WEX:BTCRUR"
+							},
+							{
+							"s": "CME:BTC1!"
+							}
+						],
+						"title": "Bitcoin"
+						},
+						{
+						"title_raw": "Ethereum",
+						"symbols": [
+							{
+							"s": "COINBASE:ETHUSD"
+							},
+							{
+							"s": "KRAKEN:ETHEUR"
+							},
+							{
+							"s": "KRAKEN:ETHGBP"
+							},
+							{
+							"s": "KRAKEN:ETHJPY"
+							},
+							{
+							"s": "POLONIEX:ETHBTC"
+							},
+							{
+							"s": "WEX:ETHLTC"
+							}
+						],
+						"title": "Ethereum"
+						},
+						{
+						"title_raw": "Bitcoin Cash",
+						"symbols": [
+							{
+							"s": "COINBASE:BCHUSD"
+							},
+							{
+							"s": "BITSTAMP:BCHEUR"
+							},
+							{
+							"s": "CEXIO:BCHGBP"
+							},
+							{
+							"s": "POLONIEX:BCHBTC"
+							},
+							{
+							"s": "HITBTC:BCHETH"
+							},
+							{
+							"s": "WEX:BCHLTC"
+							}
+						],
+						"title": "Bitcoin Cash"
+						}
+					],
+					"plotLineColorFalling": "rgba(255, 74, 104, 1)",
+					"plotLineColorGrowing": "rgba(60, 188, 152, 1)",
+					"showChart": true,
+					"title_link": "/markets/cryptocurrencies/prices-all/",
+					"locale": "en",
+					"symbolActiveColor": "rgba(242, 250, 254, 1)",
+					"belowLineFillColorFalling": "rgba(255, 74, 104, 0.05)",
+					"height": 500,
+					"width": 450
+					}
+					</script>
